19e5fb1b4fd7a01b413a66d203c7e5aa13bbe60e
xaitov.ixtiyor <xaitov.ixtiyor@aloqabank.uz>
2024-04-05 15:28:10 +0500

    first commit

---
+++ src/main/java/dev/ikhtiyor/springrocksdb/SpringRocksdbApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.springrocksdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRocksdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRocksdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ikhtiyor/springrocksdb/SpringRocksdbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.springrocksdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRocksdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
